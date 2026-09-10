503f429b2eaf078fa695f5c9845d5c10a79cde5f
tugamars <25794492+tugamars@users.noreply.github.com>
2026-09-10 19:44:03 +0100

    Updated Warrant flow

--- web/src/components/management/ManagementPermissions.svelte
+++ web/src/components/management/ManagementPermissions.svelte
@@ -11,8 +11,8 @@
 	const EMS_HIDDEN_CATEGORIES = ['bolos', 'vehicles', 'weapons', 'cases', 'evidence', 'warrants', 'charges'];
 	/** Individual permissions hidden for EMS within visible categories */
 	const EMS_HIDDEN_PERMISSIONS = ['cameras_view', 'management_tracking'];
-	const DOJ_VISIBLE_CATEGORIES = ['citizens', 'cases', 'evidence', 'charges', 'management'];
-	const DOJ_HIDDEN_PERMISSIONS = ['management_bulletins', 'management_activity', 'management_tags', 'management_tracking', 'management_settings', 'citizens_edit_licenses'];
+	const DOJ_VISIBLE_CATEGORIES = ['citizens', 'cases', 'evidence', 'charges', 'warrants', 'management'];
+	const DOJ_HIDDEN_PERMISSIONS = ['management_bulletins', 'management_activity', 'management_tags', 'management_tracking', 'management_settings', 'citizens_edit_licenses', 'warrants_view', 'warrants_issue', 'warrants_approve', 'warrants_close'];
 
 	let visibleCategories = $derived.by(() => {
 		const builtInCategoryKeys = new Set(PERMISSION_CATEGORIES.map(category => category.key));
